a57069039353114458bf4e4be5ae28f767ae2de2
AbdelrahmanAshraf9079 <abdelrahman9709@gmail.com>
2020-05-22 03:23:09 +0200

    2ndcommit

--- fixtures/config.sh
+++ fixtures/config.sh
@@ -3,18 +3,34 @@ export FABRIC_CFG_PATH=$PWD
 rm -rf artifacts/*
 rm -rf crypto-config/
 
+docker rm -f $(docker ps -aq)
+
 ./bin/cryptogen generate --config=./crypto-config.yaml
 
-./bin/configtxgen -profile OrdererGenesis -channelID Orderer-channel -outputBlock ./artifacts/genesis.block
+./bin/configtxgen -profile OrdererGenesis -channelID Orderer-channel -outputBlock ./channel-artifacts/genesis.block
 
 
-./bin/configtxgen -profile MainChannel -outputCreateChannelTx ./artifacts/MainChannel.tx -channelID MainChannel
+./bin/configtxgen -profile MainChannel -outputCreateChannelTx ./channel-artifacts/MainChannel.tx -channelID mainchannel
 
 
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./artifacts/SalesMSPanchors.tx -channelID MainChannel -asOrg SalesMSP
+./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/SalesMSPanchors.tx -channelID mainchannel -asOrg SalesMSP
 
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./artifacts/ResourcingMSPanchors.tx -channelID MainChannel -asOrg ResourcingMSP
+./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/ResourcingMSPanchors.tx -channelID mainchannel -asOrg ResourcingMSP
 
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./artifacts/EngManagementMSPanchors.tx -channelID MainChannel -asOrg EngManagementMSP
+./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/EngManagementMSPanchors.tx -channelID mainchannel -asOrg EngManagementMSP
 
-./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./artifacts/UpManagementMSPanchors.tx -channelID MainChannel -asOrg UpManagementMSP
+./bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channel-artifacts/UpManagementMSPanchors.tx -channelID mainchannel -asOrg UpManagementMSP
+
+
+
+export IMAGE_TAG=latest
+
+docker-compose -f docker-compose-cli.yaml -f docker-compose-etcdraft2.yaml up -d
+
+# docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml -f docker-compose-etcdraft2.yaml up -d
+
+docker ps -a
+
+docker exec -it cli bash
+
+
